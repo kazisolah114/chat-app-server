601d17d7956c830743d25352022100ea145750cc
Kazi Solayman <kazisolayman123@gmail.com>
2025-12-19 11:49:07 -0600

    Updated server route into an api route and added env

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
-import connectDb from './config/connectDb.js';
-import userRoute from './routes/userRoute.js';
-import messageRoute from './routes/messageRoute.js';
+import connectDb from '../config/connectDb.js';
+import userRoute from '../routes/userRoute.js';
+import messageRoute from '../routes/messageRoute.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { app, server } from './socket/socket.js'
+import { app, server } from '../socket/socket.js'
 
 // Middlewear
 configDotenv();
@@ -19,6 +19,9 @@ app.use(cors(corsOption));
 
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+})
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
@@ -26,3 +29,4 @@ server.listen(PORT, () => {
     connectDb();
     console.log(`The server is running on port http://localhost:${PORT}`)
 })
+export default { server, app };
